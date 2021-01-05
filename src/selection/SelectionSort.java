8e6046e6ab3c747b634b0497be710b7056ae9096
guoyihua <1573338993@qq.com>
2021-01-06 00:14:48 +0800

    选择排序

--- src/selection/SelectionSort.java
+++ src/selection/SelectionSort.java
@@ -1,6 +1,7 @@
 package selection;
 
-import java.util.Arrays;
+import util.ArrayGenerator;
+import util.SortedUtil;
 
 /**
  * @description: 选择排序
@@ -9,7 +10,12 @@ import java.util.Arrays;
  */
 public class SelectionSort {
 
+    private SelectionSort() {
+    }
+
     /**
+     * 循环不变量arr[i...n) 是无序的; arr[0...i) 是有序的
+     * <p>
      * 这块做了一个限制，只有实现了Comparable接口的类才可以作为方法参数来做比较
      *
      * @param arr
@@ -29,6 +35,26 @@ public class SelectionSort {
         return arr;
     }
 
+    /**
+     * 循环不变量arr[i...n) 是有序的; arr[0...i) 是无序的
+     *
+     * @param arr
+     * @param <E>
+     * @return
+     */
+    public static <E extends Comparable> E[] sort2(E[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int maxIndex = i;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j].compareTo(arr[maxIndex]) > 0) {
+                    maxIndex = j;
+                }
+            }
+            swap(arr, i, maxIndex);
+        }
+        return arr;
+    }
+
     private static <E> void swap(E[] arr, int target, int minIndex) {
         E temp = arr[target];
         arr[target] = arr[minIndex];
@@ -36,16 +62,14 @@ public class SelectionSort {
     }
 
     public static void main(String[] args) {
-        Integer[] arr = {2, 0, 3, 5, 4, 1};
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
-
-        Student[] students = {
-                new Student("张三", 1, 85),
-                new Student("李四", 10, 80),
-                new Student("王五", 5, 100),
-        };
-        sort(students);
-        System.out.println(Arrays.toString(students));
+        int[] counts = {10000, 100000};
+        for (int n : counts) {
+            Integer[] arr = ArrayGenerator.generateRandom(n, n);
+            try {
+                SortedUtil.sortTest(SelectionSort.class.getName(), arr);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
